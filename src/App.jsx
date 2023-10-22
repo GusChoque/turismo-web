b37579cb404eb7f71b9af8cbf581ea77dfd5acf0
GusChoque <110579994+GusChoque@users.noreply.github.com>
2023-10-22 16:03:33 -0300

    react router fail hahaha

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import React from 'react'
-import Footer from './Components/Footer'
-import Home from './Components/Home'
-import NavBar from './Components/NavBar'
-import Travels from './Components/Travels'
+import Main from "./Components/Main";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <NavBar />
-      <Home />
-      <Travels />
-      <Footer />
-    </React.Fragment>
+    <Main />
   )
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import { Link, BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
+import Main from "./Main";
 
 function NavBar() {
   return (
-    <nav className="w-full inline-flex justify-between p-6 barra">
-      <h1 className="text-4xl font-normal leading-10 py-2 px-4">Turismo web</h1>
-      <ul className="inline-flex text-center grow items-center">
-        <li className="w-28 p-2">
-          <span className="text-base font-normal ">Home</span>
-        </li>
-        <li className="w-28 p-2">
-          <span className="text-base font-normal">About us</span>
-        </li>
-        <li className="w-28 p-2">
-          <span className="text-base font-normal">Contact</span>
-        </li>
-      </ul>
-      <div className="text-base font-bold p-4">Lang</div>
-    </nav>
+    <BrowserRouter>
+      <nav className="w-full inline-flex justify-between p-6 barra">
+        <h1 className="text-4xl font-normal leading-10 py-2 px-4">
+          Turismo web
+        </h1>
+        <ul className="inline-flex text-center grow items-center">
+          <li className="w-28 p-2">
+            <Link to="/" className="text-base font-normal">
+              Home
+            </Link>
+          </li>
+          <li className="w-28 p-2">
+            <Link to="/about" className="text-base font-normal">
+              About us
+            </Link>
+          </li>
+          <li className="w-28 p-2">
+            <Link to="/contact" className="text-base font-normal">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="text-base font-bold p-4">Lang</div>
+      </nav>    
+    </BrowserRouter>
   );
 }
 
