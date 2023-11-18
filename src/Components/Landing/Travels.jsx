ba70edea81f4dae5ac14216641cb65010715cc30
Gustavo Choque <gustavochoque0791@gmail.com>
2023-11-18 18:24:50 -0300

    added bg and link btn changed styles

--- src/Components/Landing/Travels.jsx
+++ src/Components/Landing/Travels.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { Recorridos } from "../../assets/locales/Recorridos";
+import image1 from "../../assets/images/image1.jpg";
+import image2 from "../../assets/images/image2.jpg";
+import image3 from "../../assets/images/image3.jpg";
 
 const formatName = (name) => {
   return name.toLowerCase().replace(/\s+/g, "-");
@@ -7,31 +10,44 @@ const formatName = (name) => {
 
 const Travels = () => {
   return (
-    <section className="py-4 px-8 bg-local mb-6 travels">
-      <h2 className="text-4xl font-bold text-center py-10">
+    <section
+      className="py-10 px-8 bg-local travels"
+      style={{
+        backgroundImage: `url(${image2})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <h2 className="text-4xl font-bold text-center pb-10">
         Get to Know Salta
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto max-w-screen-xl">
         {Recorridos.map((recorrido) => {
           const formattedName = formatName(recorrido.name);
           return (
-            <Link key={formattedName} to={`/${formattedName}`}>
-              <article className="bg-white border p-8 rounded-lg shadow-lg">
+            <article
+              className="bg-white rounded-lg flex flex-col shadow-black shadow-2xl overflow-clip gap-4 pt-8"
+              key={formattedName}
+            >
+              <div className="px-8">
                 <h2 className="text-2xl font-semibold pb-4">
                   {recorrido.name}
                 </h2>
-                <p className="text-justify">{recorrido.description}</p>
-                <p className="p-2">Duration: {recorrido.duration}</p>
+                <p className="text-justify text-lg">{recorrido.description}</p>
+                <p className="py-4">Duration: {recorrido.duration}</p>
+              </div>
+                <Link
+                  to={`/${formattedName}`}
+                  className="p-1 text-blac text-lg font-bold rounded-md hover:text-white bg-orange-300 hover:bg-orange-500 duration-300 inline-block self-center"
+                >
+                  Mas info
+                </Link>
 
-                <div className="flex justify-center items-center h-96 my-2">
-                  <img
-                    src={recorrido.image}
-                    alt={`${recorrido.name}`}
-                    className="mt-6 rounded-lg object-cover max-h-96 shadow-md"
-                  />
-                </div>
-              </article>
-            </Link>
+              <img
+                src={recorrido.image}
+                alt={`${recorrido.name}`}
+                className="rounded-b-lg w-full flex justify-center items-center h-96 hover:scale-110 duration-500"
+              />
+            </article>
           );
         })}
       </div>
