3354e8333170fc2257dc4155820bf378f617bdc3
Gustavo Choque <gustavochoque0791@gmail.com>
2023-11-18 18:25:10 -0300

    changed home style

--- src/Components/Landing/Home.jsx
+++ src/Components/Landing/Home.jsx
@@ -28,8 +28,8 @@ function Home() {
         className={`max-w-screen-xl mx-auto h-[80vh] sm:h-[65vh] py-4 bg-local home relative`}
       >
         <div className="flex flex-col text-center items-center justify-center h-full">
-          <h1 className="py-10 text-5xl font-bold ">Explore Salta with us</h1>
-          <p className="p-5 text-base capitalize sm:w-2/3 font-medium">
+          <h1 className="py-10 text-5xl font-bold text-white">Explore Salta with us</h1>
+          <p className="p-5 text-lg capitalize sm:w-2/3 font-medium text-white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
             voluptatum perferendis? Labore sed animi ad soluta quasi
             necessitatibus corrupti illum blanditiis, dolor et quam? Nemo rem
