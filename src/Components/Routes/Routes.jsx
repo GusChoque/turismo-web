4475a74592f4418ac990e59d71d0256e8da30780
Gustavo Choque <gustavochoque0791@gmail.com>
2023-11-19 17:27:43 -0300

    same as before

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Pages/Main/Main";
-import Home from "../Pages/Landing/Home";
 import Travels from "../Pages/Landing/Travels"
 import About from "../Pages/About/AboutUs"
 import Contact from "../Pages/Contact/Contact"
