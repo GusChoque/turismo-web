22ea09e9c1ba217dca8f92b9ce14294841698ba2
Gustavo Choque <gustavochoque0791@gmail.com>
2023-11-21 19:30:35 -0300

    email send implemented

---
+++ env.js
@@ -0,0 +1,4 @@
+export const VITE_MAP_COORD = JSON.parse(import.meta.env.VITE_MAP_COORD);
+export const VITE_MAIL_SERVICE_ID = import.meta.env.VITE_MAIL_SERVICE_ID;
+export const VITE_MAIL_TEMPLATE_ID = import.meta.env.VITE_MAIL_TEMPLATE_ID;
+export const VITE_MAIL_USER_ID = import.meta.env.VITE_MAIL_USER_ID;
--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,58 +1,83 @@
-import Map
- from "../../Utils/Map";
+import Map from "../../Utils/Map/Map";
+import emailjs from "@emailjs/browser";
+import { VITE_MAIL_USER_ID, VITE_MAIL_SERVICE_ID, VITE_MAIL_TEMPLATE_ID } from "../../../../env";
+
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(VITE_MAIL_SERVICE_ID, VITE_MAIL_TEMPLATE_ID, e.target, VITE_MAIL_USER_ID)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
     <section className="py-10 px-8 bg-local contact">
       <div className="max-w-screen-xl mx-auto flex flex-col items-center">
-        <h1 className="text-4xl font-bold text-center text-white mb-8">
+        <h1 className="text-4xl font-bold text-center mb-8">
           Contact
         </h1>
-        <div className="flex flex-col sm:flex-row w-full">
-        <div className="sm:block sm:w-1/2 mt-4 sm:mt-0 p-4">
-          <Map />
+        <div className="flex flex-col sm:flex-row w-full gap-4">
+          <div className="sm:block sm:w-1/2 mt-4 sm:mt-0 shadow-2xl rounded-lg">
+            <Map />
           </div>
-          <div className="w-full sm:w-1/2 p-4">
-            <form className="bg-white p-6 rounded-lg shadow-lg">
+          <div className="w-full sm:w-1/2">
+            <form className=" p-6 rounded-lg shadow-2xl border-2 border-gray-400" onSubmit={sendEmail}>
               <h2 className="text-2xl font-semibold mb-4">Contact us here</h2>
               <div className="mb-4">
-                <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
+                <label
+                  htmlFor="user_name"
+                  className="block text-gray-700 font-medium mb-2"
+                >
                   Name
                 </label>
                 <input
                   type="text"
-                  id="name"
+                  id="user_name"
+                  name="user_name"
                   placeholder="Name"
                   className="w-full border-2 border-gray-400 p-2 rounded-lg focus:outline-none focus:border-black"
+                  required
                 />
               </div>
               <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+                <label
+                  htmlFor="user_email"
+                  className="block text-gray-700 font-medium mb-2"
+                >
                   E-Mail
                 </label>
                 <input
                   type="text"
-                  id="email"
+                  id="user_email"
+                  name="user_email"
                   placeholder="E-Mail"
                   className="w-full border-2 border-gray-400 p-2 rounded-lg focus:outline-none focus:border-black"
+                  required
                 />
               </div>
               <div className="mb-4">
-                <label htmlFor="comment" className="block text-gray-700 font-medium mb-2">
+                <label
+                  htmlFor="message"
+                  className="block text-gray-700 font-medium mb-2"
+                >
                   Comment
                 </label>
                 <textarea
-                  id="comment"
+                  id="message"
                   rows="5"
                   placeholder="Comment"
+                  name="message"
                   className="w-full border-2 border-gray-400 p-2 rounded-lg focus:outline-none focus:border-black"
+                  required
                 />
               </div>
-              <button className="bg-red-500 text-white rounded-lg p-3 w-1/4 self-center hover:bg-red-600 hover:text-white">
-                Send
-              </button>
+              <input type="submit" value="Send" className="text-lg font-bold text-white bg-[#345E92] hover:bg-[#4A85CD] duration-300 rounded-lg p-3 w-1/4 self-center cursor-pointer" />
             </form>
           </div>
-          
         </div>
       </div>
     </section>
